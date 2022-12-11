2a0faf73b2c6e16b258df945b566c4d35384d92d
SinghRamanjot <86426100+SinghRamanjot@users.noreply.github.com>
2022-12-11 12:39:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Alert from "./components/Alert";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
